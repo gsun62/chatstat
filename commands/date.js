c2e29b9a00407b261c0f763c68cc2bc5a019416b
Gabriel <gabrielsun03@yahoo.com>
2022-08-16 11:35:39 +0200

    Restructure directories to faciliate command creation

---
+++ commands/date.js
@@ -0,0 +1,15 @@
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('date')
+    .setDescription('Reports the current date.'),
+  async execute(interaction) {
+    const d = new Date();
+    const date = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
+    return interaction.reply(date);
+  },
+};
+
+
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -10,19 +22,16 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	} else if (commandName === 'date') {
-    const d = new Date();
-    const date = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
-    await interaction.reply(date);
-  }
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 client.login(token);
